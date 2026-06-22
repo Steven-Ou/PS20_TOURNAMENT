42def9db3721c9d6ae3f259963fd54d488d973fe
Steven Ou <osteve425@gmail.com>
2026-06-21 20:49:04 -0400

    adding in the url and the key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project-id.supabase.co'; // Replace with your URL
-const supabaseAnonKey = 'your-anon-public-key';           // Replace with your Key
+const supabaseUrl = 'https://wtzwjqxddtygsiqusvzi.supabase.co'; // Replace with your URL
+const supabaseAnonKey = 'sb_publishable_1RgC5CcXtvLd2iSZcU3n3A_OsusXmqf';           // Replace with your Key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
